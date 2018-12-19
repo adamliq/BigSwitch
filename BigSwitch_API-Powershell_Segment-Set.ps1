3cd6558f29fdabb0b902ac4ce114dd24a5d449d4
adamliq <adamliq@hotmail.com>
2018-12-19 15:57:39 +1100

    Create BigSwitch_API-Powershell_Segment-Set.ps1

---
+++ BigSwitch_API-Powershell_Segment-Set.ps1
@@ -0,0 +1,32 @@
+function Set-BCFSegment([string]$tenant,[string]$segment)
+{
+    #Conduct Action on BCF Using token
+    $api_tenant_name_segment="https://"+$IP +":8443/api/v1/data/controller/applications/bcf/tenant[name=" + '"' + $tenant + '"' + "]/segment"
+    $api_segment_verify_name="https://"+$IP +":8443/api/v1/data/controller/applications/bcf/tenant[name=" + '"' + $tenant + '"' + "]/segment[name=" + '"' + $segment + '"' + "]"
+	
+    $params=@{"name"= $segment} |convertto-json -compress
+    $result1 = Invoke-WebRequest -Uri $api_tenant_name_segment -Method Post -Body $params -UseBasicParsing -WebSession $myWebSession
+
+    #Check status of http
+    if ($result1.StatusCode -eq 204)
+    {
+        write-host "**PASS, statuscode $($result1.StatusCode) returned successfully" -ForegroundColor Yellow
+        Start-Sleep 5
+        #Start Array
+        $segmentname_get = Invoke-WebRequest -Uri $api_segment_verify_name -Method get -UseBasicParsing -WebSession $myWebSession
+        if ($segmentname_get.Content -ne "[ ]")
+        {
+            $jsonoutput_segmentname=$segmentname_get.Content | ConvertFrom-Json
+            Write-Host "**Verified: Segment $($jsonoutput_segmentname.name)" -ForegroundColor Green 
+        }
+        else
+        {
+        "$segment Not found"
+        }
+    }
+    else
+    {
+        write-host "**FAIL, statuscode $($result1.StatusCode) returned successfully" -ForegroundColor Yellow
+        return
+    }
+}
