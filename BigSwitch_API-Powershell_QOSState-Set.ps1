c244cf64a91def406f3aa16b2d3a5148ae4994d6
adamliq <adamliq@hotmail.com>
2018-12-19 16:00:17 +1100

    Create BigSwitch_API-Powershell_QOSState-Set.ps1

---
+++ BigSwitch_API-Powershell_QOSState-Set.ps1
@@ -0,0 +1,34 @@
+Function Set-BCFQOS([string]$QOS_State)
+{
+    $params2=@{"enable"=$QOS_State} |convertto-json -compress
+    #Conduct Action on BCF Using token
+    $api_QOS="https://"+$IP +":8443/api/v1/data/controller/applications/bcf/global-setting/qos"
+
+    $result1 = Invoke-WebRequest -Uri $api_QOS -Method post -body $params2 -UseBasicParsing -WebSession $myWebSession
+
+    #Check status of http
+    if ($result1.StatusCode -eq 204)
+    {
+        write-host "**PASS, statuscode $($result1.StatusCode) returned successfully" -ForegroundColor Yellow
+        Start-Sleep 5
+        #Start Array
+        $jsonoutput_segmentname=$result1.Content | ConvertFrom-Json
+        $jsonoutput_segmentname|ft
+        #Conduct Action on BCF Using token
+        $tenant_segment_interface="https://"+$IP +":8443/api/v1/data/controller/applications/bcf/global-setting/qos"
+        $result1 = Invoke-WebRequest -Uri $tenant_segment_interface -Method get -UseBasicParsing -WebSession $myWebSession
+        if((($result1.content|convertfrom-json).enable) -eq "True")
+        {
+            write-host "**Verified: QOS is enabled" -ForegroundColor Green
+        }
+        else
+        {
+
+        }
+    }
+    else
+    {
+        write-host "**FAIL, statuscode $($result1.StatusCode) returned successfully" -ForegroundColor Yellow
+        return
+    }
+}
